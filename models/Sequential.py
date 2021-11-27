40da37bc08df447a0a12af48b54a61371f4e9291
Cole Horvat <ColeHorvat@gmail.com>
2021-11-27 14:51:26 -0800

    Sequential now functioning

--- models/Sequential.py
+++ models/Sequential.py
@@ -12,8 +12,9 @@ def processSequential(X_train, X_test, y_train, y_test):
 
     #Change the layers and hidden units (first argument) during testing
     #https://keras.io/guides/sequential_model/
+    model.add(Dense(32, activation='relu'))
     model.add(Dense(8, activation='relu'))
-    model.add(Dense(8, activation='relu'))
+    model.add(Dense(4, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
     #Experiement with different arguments during testing
@@ -22,9 +23,13 @@ def processSequential(X_train, X_test, y_train, y_test):
               metrics=['accuracy'])
     
     
-    model.fit(X_train, y_train, epochs=150, batch_size=1, verbose=1)
+    model.fit(X_train, y_train, epochs=4, batch_size=1, verbose=1)
 
-    predictions = model.predict_classes(X_train)
+    _, accuracy = model.evaluate(X_test, y_test)
+    print('Accuracy: %.2f' % (accuracy*100))
 
-    for i in (len(X_train)):
-        print('%s => %d (expected %d)' % (X_train[i].tolist(), predictions[i], y_train[i]))
+
+    # predictions = model.predict_classes(X_train)
+
+    # for i in (len(X_train)):
+    #     print('%s => %d (expected %d)' % (X_train[i].tolist(), predictions[i], y_train[i]))
